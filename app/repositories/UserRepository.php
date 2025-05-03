9f4198f75b10d37e15e40a137cd4acaed6b041d0
zash <zash22@gmail.com>
2025-05-03 22:11:55 +0200

    Created Repository layer to get data for service layer

---
+++ app/repositories/UserRepository.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+class UserRepository
+{
+    public function __construct()
+    {
+
+    }
+
+    public function getAllUsers(): array
+    {
+        return [];
+    }
+
+    public function getUserById(int $id): ?array
+    {
+        return [];
+    }
+}
