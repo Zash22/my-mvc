447363f6887b8105c3ab64c657380da6496fe92c
zash <zash22@gmail.com>
2025-05-03 19:33:21 +0200

    mvc scaffolding

---
+++ app/models/Home.php
@@ -0,0 +1,9 @@
+<?php
+
+class Home extends Model
+{
+    public function getData()
+    {
+        return "Hello from the model!";
+    }
+}
---
+++ app/views/home/index.php
@@ -0,0 +1,2 @@
+<h1><?= $data['title'] ?></h1>
+<p>Hello from the View!</p>
