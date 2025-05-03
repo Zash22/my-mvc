46d76d8655ba311f2d11fe5aa185c0add6a14174
zash <zash22@gmail.com>
2025-05-03 23:01:08 +0200

    dummy data to get the views working

--- app/repositories/UserRepository.php
+++ app/repositories/UserRepository.php
@@ -10,11 +10,23 @@ class UserRepository
 
     public function getAllUsers(): array
     {
-        return [];
+        $users[0]['name'] = 'Lester';
+        $users[0]['email'] = 'lester@tester.com';
+
+        $users[1]['name'] = 'Mister';
+        $users[1]['email'] = 'mister@lister.com';
+
+        return $users;
     }
 
     public function getUserById(int $id): ?array
+
     {
-        return [];
+        $users['id'] = $id;
+        $users['name'] = 'Lester';
+        $users['email'] = 'lester@tester.com';
+
+        return $users;
+
     }
 }
